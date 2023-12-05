a1ebe70dad81b8d1a3b33466638b895e90f225ca
MADAO-999 <madao_999@163.com>
2023-12-05 10:25:00 +0800

    项目初始化

---
+++ src/main/java/com/taoaipan/TaoAiPanApplication.java
@@ -0,0 +1,24 @@
+package com.taoaipan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @version 1.0
+ * @Author TaoAi
+ * @Date 2023/12/5 9:39
+ * @description 主类
+ */
+@EnableAsync
+@EnableScheduling
+@EnableTransactionManagement
+@SpringBootApplication(scanBasePackages = {"com.taoaipan"})
+public class TaoAiPanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaoAiPanApplication.class, args);
+    }
+
+}
